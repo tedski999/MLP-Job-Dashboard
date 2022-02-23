f4278922e52de43fd095b2b5404e695c0a811ab4
tedski999 <tedjohnsonjs@gmail.com>
2022-02-23 17:19:57 +0000

    Add example fetch of server API
    The CoolComponent now uses a state variable called requestedData.
    Initially, this variable is set to "Loading...".
    A fetch is made to our API endpoint /v1/my_cool_endpoint and
    the response of this fetch is used to update requestedData.
    This will automatically re-render the CoolComponent.
    
    Try changing the fetched URL to something invalid to see how error
    handling works. Also try changing what the server responds. You can
    find that code on line 13 in /src/server/index.js

--- src/client/MyCoolComponent/TestComponent.js
+++ src/client/MyCoolComponent/TestComponent.js
@@ -2,12 +2,47 @@ import React from "react";
 import logo from "./logo.png";
 
 class CoolComponent extends React.Component {
+
+	constructor(props) {
+		super(props);
+
+		// Set the initial state
+		this.state = {
+			requestedData: "Loading..."
+		};
+
+		// Make a request to our API
+		fetch("/v1/my_cool_endpoint")
+
+			// Once the response has been received...
+			.then(response => {
+				// Throw an error if we don't get an OK response
+				if (!response.ok)
+					throw "Error " + response.status;
+				// Otherwise, parse the response into a string
+				return response.text();
+			})
+
+			// Once the response has been parsed...
+			.then(data => {
+				// Update our React state with the received data
+				this.setState({requestedData: "API response: " + data});
+			})
+
+			// If any exceptions occur...
+			.catch(err => {
+				// Update our React state with the error
+				this.setState({requestedData: "Request failed: " + err});
+			});
+	}
+
 	render() {
 		const message = "Hello, React.js!";
 		return (
 			<div className="CoolComponentStyling">
 				<h3>{message}</h3>
 				<img src={logo} />
+				<p>{this.state.requestedData}</p>
 			</div>
 		);
 	}
