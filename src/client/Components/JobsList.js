76650936afbb0a57c9b5b49917cc174c403e2a41
aoife-K <aoifekhan17@gmail.com>
2022-03-12 21:00:36 +0000

    Simple list implemented, no styling yet

--- src/client/Components/JobsList.js
+++ src/client/Components/JobsList.js
@@ -1,8 +1,29 @@
 import React from "react";
+// import '@coreui/coreui/dist/css/coreui.min.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
+import { CListGroup } from '@coreui/react';
+import { CListGroupItem } from '@coreui/react';
 
 function JobsList(){
 	return <div className="Jobs-List">
         JOBS LIST PAGE
+		<CListGroup>
+			<CListGroupItem component="a" href="#" active>
+				Cras justo odio
+			</CListGroupItem>
+			<CListGroupItem component="a" href="#">
+				Dapibus ac facilisis in
+			</CListGroupItem>
+			<CListGroupItem component="a" href="#">
+				Morbi leo risus
+			</CListGroupItem>
+			<CListGroupItem component="a" href="#">
+				Porta ac consectetur ac
+			</CListGroupItem>
+			<CListGroupItem component="a" href="#" disabled>
+				Vestibulum at eros
+			</CListGroupItem>
+		</CListGroup>
 	</div>;
 }
 export default JobsList;
