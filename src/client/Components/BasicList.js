086605b1922836809e3346d40ddd0076aba9207e
aoife-K <aoifekhan17@gmail.com>
2022-03-14 15:39:28 +0000

    Added styling and using simple data

---
+++ src/client/Components/BasicList.js
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+// import InboxIcon from '@mui/icons-material/Inbox';
+// import DraftsIcon from '@mui/icons-material/Drafts';
+
+export default function BasicList() {
+  return (
+    <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+      <nav aria-label="main mailbox folders">
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemText primary="Verify Remote Backup" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemText primary="Full System Backup to Local Storage" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </nav>
+      {/* <Divider /> */}
+      <nav aria-label="secondary mailbox folders">
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemText primary="Full System Backup to Local Storage" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simple-list">
+              <ListItemText primary="Verify Local Backup" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </nav>
+    </Box>
+  );
+}
--- src/client/Components/JobsList.js
+++ src/client/Components/JobsList.js
@@ -1,29 +1,12 @@
 import React from "react";
-// import '@coreui/coreui/dist/css/coreui.min.css';
-//import 'bootstrap/dist/css/bootstrap.min.css';
-import { CListGroup } from '@coreui/react';
-import { CListGroupItem } from '@coreui/react';
+import BasicList from "./BasicList";
+import AlignItemsList from "./AlignItemsList";
 
 function JobsList(){
 	return <div className="Jobs-List">
         JOBS LIST PAGE
-		<CListGroup>
-			<CListGroupItem component="a" href="#" active>
-				Cras justo odio
-			</CListGroupItem>
-			<CListGroupItem component="a" href="#">
-				Dapibus ac facilisis in
-			</CListGroupItem>
-			<CListGroupItem component="a" href="#">
-				Morbi leo risus
-			</CListGroupItem>
-			<CListGroupItem component="a" href="#">
-				Porta ac consectetur ac
-			</CListGroupItem>
-			<CListGroupItem component="a" href="#" disabled>
-				Vestibulum at eros
-			</CListGroupItem>
-		</CListGroup>
+		{/* <div><BasicList></BasicList></div> */}
+		<div><AlignItemsList></AlignItemsList></div>
 	</div>;
 }
 export default JobsList;
