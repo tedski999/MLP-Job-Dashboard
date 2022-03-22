881719f0364cbe1c5428a37907b655678ab55542
AlannaFusciardiWallace <81378350+AlannaFusciardiWallace@users.noreply.github.com>
2022-03-22 14:18:46 +0000

    fixed props

--- src/client/Components/LineGraph.js
+++ src/client/Components/LineGraph.js
@@ -1,8 +1,14 @@
 import React from "react";
 import Chart from "react-apexcharts";
-
+import PropTypes from "prop-types";
 
 function LineGraph(props){
+
+	LineGraph.propTypes = {
+		options: PropTypes.object,
+		series: PropTypes.array
+	};
+	
 	return <div className="Jobs-List">
 		<Chart 
 			options={props.options}
