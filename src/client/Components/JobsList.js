c2789575c022059d09ac4e21fbd32bd22f23148c
aoife-K <aoifekhan17@gmail.com>
2022-03-27 13:00:22 +0100

    Nicer job list implemented, simple data so far

--- src/client/Components/JobsList.js
+++ src/client/Components/JobsList.js
@@ -2,12 +2,14 @@ import React from "react";
 import BasicList from "./BasicList";
 import AlignItemsList from "./AlignItemsList";
 import List from "./List";
+import EnhancedTable from "./EnhancedTabled";
 
 function JobsList(){
 	return <div className="Jobs-List">
         JOBS LIST PAGE
 		{/* <div><AlignItemsList></AlignItemsList></div> */}
-		<div><List></List></div>
+		{/* <div><List></List></div> */}
+		<div><EnhancedTable></EnhancedTable></div>
 	</div>;
 }
 export default JobsList;
