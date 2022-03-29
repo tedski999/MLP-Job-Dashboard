0fb417c4f7f4d324b135fc092aa00a942435431e
jissj9 <jjojoseph12@gmail.com>
2022-03-29 14:13:32 +0100

    Fixed Navbar Line Graph Route

--- src/client/Components/NavigationBar.js
+++ src/client/Components/NavigationBar.js
@@ -9,7 +9,7 @@ function NavigationBar() {
 				<div className="side side-left">
 					<NavLink to="/" className={navInfo => navInfo.isActive? "active" : ""}>Home </ NavLink>
 					<NavLink to="/jobsList" className="button">Jobs List </NavLink>
-					<NavLink to="/line" className="navbar-btn">About </NavLink>
+					<NavLink to="/line" className="navbar-btn">Line Graph </NavLink>
 					<button onClick={() => navigate(-1)} title="Go previous page">Back </button>
 				</div>
 			</div>
