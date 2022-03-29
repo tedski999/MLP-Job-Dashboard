fa4ff326b4920983a54c4a385aa6628558107d8b
jissj9 <jjojoseph12@gmail.com>
2022-03-29 14:07:17 +0100

    Added navigation bar component

---
+++ src/client/Components/NavigationBar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {NavLink, useNavigate } from "react-router-dom";
+
+function NavigationBar() {
+	const navigate = useNavigate();
+	return (
+		<div className="navigation">
+			<div className="navigation-bar">
+				<div className="side side-left">
+					<NavLink to="/" className={navInfo => navInfo.isActive? "active" : ""}>Home </ NavLink>
+					<NavLink to="/jobsList" className="button">Jobs List </NavLink>
+					<NavLink to="/line" className="navbar-btn">About </NavLink>
+					<button onClick={() => navigate(-1)} title="Go previous page">Back </button>
+				</div>
+			</div>
+		</div>
+	);
+}
+export default NavigationBar;
