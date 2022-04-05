36e12819c99bfcc227cb16bc65a36aa7085d8add
jissj9 <jjojoseph12@gmail.com>
2022-04-05 16:29:15 +0100

    Fix Table header cells Linting

--- src/client/Components/EnhancedTabled.js
+++ src/client/Components/EnhancedTabled.js
@@ -1,27 +1,25 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import { alpha } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TablePagination from '@mui/material/TablePagination';
-import TableRow from '@mui/material/TableRow';
-import TableSortLabel from '@mui/material/TableSortLabel';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Paper from '@mui/material/Paper';
-import Checkbox from '@mui/material/Checkbox';
-import IconButton from '@mui/material/IconButton';
-import Tooltip from '@mui/material/Tooltip';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Switch from '@mui/material/Switch';
-import DeleteIcon from '@mui/icons-material/Delete';
-import FilterListIcon from '@mui/icons-material/FilterList';
-import { visuallyHidden } from '@mui/utils';
-import { withRouter } from 'react-router-dom';
+import * as React from "react";
+import PropTypes from "prop-types";
+import { alpha } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+import TableSortLabel from "@mui/material/TableSortLabel";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
+import IconButton from "@mui/material/IconButton";
+import Tooltip from "@mui/material/Tooltip";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Switch from "@mui/material/Switch";
+import DeleteIcon from "@mui/icons-material/Delete";
+import FilterListIcon from "@mui/icons-material/FilterList";
+import { visuallyHidden } from "@mui/utils";
 import { Link } from "react-router-dom";
 
 function createData(name, topic, jobID, lastUpdated) {
@@ -34,12 +32,10 @@ function createData(name, topic, jobID, lastUpdated) {
 }
 
 const rows = [
-	createData('Verify Local Backup', 305, 3, 67),
-	createData('Verify Remote Backup', 452, 25.0, 51),
-	createData('Full System Backup to Remote Storage', 262, 16.0, 24),
-	createData('Full System Backup to Local Storage', 159, 6.0, 24),
-	//createData('Verify Local Backup', 356, 16.0, 49),
-	//createData('Verify Remote Backup', 408, 3.2, 87),
+	createData("Verify Local Backup", 305, 3, 67),
+	createData("Verify Remote Backup", 452, 25.0, 51),
+	createData("Full System Backup to Remote Storage", 262, 16.0, 24),
+	createData("Full System Backup to Local Storage", 159, 6.0, 24),
 ];
 
 function descendingComparator(a, b, orderBy) {
@@ -53,7 +49,7 @@ function descendingComparator(a, b, orderBy) {
 }
 
 function getComparator(order, orderBy) {
-	return order === 'desc'
+	return order === "desc"
 		? (a, b) => descendingComparator(a, b, orderBy)
 		: (a, b) => -descendingComparator(a, b, orderBy);
 }
