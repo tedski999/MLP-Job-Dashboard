045517ff5b947e0cb20ce6007752182ce566aed6
aoife-K <aoifekhan17@gmail.com>
2022-04-05 14:15:54 +0100

    Add timeline component

--- src/client/Components/JobsList.js
+++ src/client/Components/JobsList.js
@@ -1,10 +1,12 @@
 import React from "react";
 import EnhancedTable from "./EnhancedTabled";
+import Timeline from "./Timeline";
 
 function JobsList(){
 	return <div className="Jobs-List">
 		JOBS LIST PAGE
 		<div><EnhancedTable></EnhancedTable></div>
+		<div><Timeline></Timeline></div>
 	</div>;
 }
 export default JobsList;
--- src/client/Components/Timeline.js
+++ src/client/Components/Timeline.js
@@ -1,368 +1,77 @@
 import * as React from 'react';
-import PropTypes from 'prop-types';
-import { alpha } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TablePagination from '@mui/material/TablePagination';
-import TableRow from '@mui/material/TableRow';
-import TableSortLabel from '@mui/material/TableSortLabel';
-import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Divider from '@mui/material/Divider';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
-import Paper from '@mui/material/Paper';
-import Checkbox from '@mui/material/Checkbox';
-import IconButton from '@mui/material/IconButton';
-import Tooltip from '@mui/material/Tooltip';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Switch from '@mui/material/Switch';
-import DeleteIcon from '@mui/icons-material/Delete';
-import FilterListIcon from '@mui/icons-material/FilterList';
-import { visuallyHidden } from '@mui/utils';
-
-function createData(name, topic, jobID, lastUpdated) {
-	return {
-		name,
-		topic,
-		jobID,
-		lastUpdated,
-	};
-}
-
-const rows = [
-	createData('Verify Local Backup', 305, 3, 67),
-	createData('Verify Remote Backup', 452, 25.0, 51),
-	createData('Full System Backup to Remote Storage', 262, 16.0, 24),
-	createData('Full System Backup to Local Storage', 159, 6.0, 24),
-	//createData('Verify Local Backup', 356, 16.0, 49),
-	//createData('Verify Remote Backup', 408, 3.2, 87),
-];
-
-function descendingComparator(a, b, orderBy) {
-	if (b[orderBy] < a[orderBy]) {
-		return -1;
-	}
-	if (b[orderBy] > a[orderBy]) {
-		return 1;
-	}
-	return 0;
-}
-
-function getComparator(order, orderBy) {
-	return order === 'desc'
-		? (a, b) => descendingComparator(a, b, orderBy)
-		: (a, b) => -descendingComparator(a, b, orderBy);
-}
-
-// This method is created for cross-browser compatibility, if you don't
-// need to support IE11, you can use Array.prototype.sort() directly
-function stableSort(array, comparator) {
-	const stabilizedThis = array.map((el, index) => [el, index]);
-	stabilizedThis.sort((a, b) => {
-		const order = comparator(a[0], b[0]);
-		if (order !== 0) {
-			return order;
-		}
-		return a[1] - b[1];
-	});
-	return stabilizedThis.map((el) => el[0]);
-}
-
-const headCells = [
-	{
-		id: 'name',
-		numeric: false,
-		disablePadding: true,
-		label: 'Job Name',
-	},
-	{
-		id: 'topic',
-		numeric: true,
-		disablePadding: false,
-		label: 'Topic',
-	},
-	{
-		id: 'jobID',
-		numeric: true,
-		disablePadding: false,
-		label: 'Job ID',
-	},
-	{
-		id: 'lastUpdated',
-		numeric: true,
-		disablePadding: false,
-		label: 'Last Updated (Mins)',
-	},
-//   {
-//     id: 'protein',
-//     numeric: true,
-//     disablePadding: false,
-//     label: 'Protein (g)',
-//   },
-];
-
-function TimelineHead(props) {
-	const { onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } =
-		props;
-	const createSortHandler = (property) => (event) => {
-		onRequestSort(event, property);
-	};
-
-	return (
-		<TableHead>
-			<TableRow>
-				<TableCell padding="checkbox">
-					<Checkbox
-						color="primary"
-						indeterminate={numSelected > 0 && numSelected < rowCount}
-						checked={rowCount > 0 && numSelected === rowCount}
-						onChange={onSelectAllClick}
-						inputProps={{
-							'aria-label': 'select all jobs',
-						}}
-					/>
-				</TableCell>
-				{headCells.map((headCell) => (
-					<TableCell
-						key={headCell.id}
-						align={headCell.numeric ? 'right' : 'left'}
-						padding={headCell.disablePadding ? 'none' : 'normal'}
-						sortDirection={orderBy === headCell.id ? order : false}
-					>
-						<TableSortLabel
-							active={orderBy === headCell.id}
-							direction={orderBy === headCell.id ? order : 'asc'}
-							onClick={createSortHandler(headCell.id)}
-						>
-							{headCell.label}
-							{orderBy === headCell.id ? (
-								<Box component="span" sx={visuallyHidden}>
-									{order === 'desc' ? 'sorted descending' : 'sorted ascending'}
-								</Box>
-							) : null}
-						</TableSortLabel>
-					</TableCell>
-				))}
-			</TableRow>
-		</TableHead>
-	);
-}
-
-TimelineHead.propTypes = {
-	numSelected: PropTypes.number.isRequired,
-	onRequestSort: PropTypes.func.isRequired,
-	onSelectAllClick: PropTypes.func.isRequired,
-	order: PropTypes.oneOf(['asc', 'desc']).isRequired,
-	orderBy: PropTypes.string.isRequired,
-	rowCount: PropTypes.number.isRequired,
-};
-
-const TimelineToolbar = (props) => {
-	const { numSelected } = props;
-
-	return (
-		<Toolbar
-			sx={{
-				pl: { sm: 2 },
-				pr: { xs: 1, sm: 1 },
-				...(numSelected > 0 && {
-					bgcolor: (theme) =>
-						alpha(theme.palette.primary.main, theme.palette.action.activatedOpacity),
-				}),
-			}}
-		>
-			{numSelected > 0 ? (
-				<Typography
-					sx={{ flex: '1 1 100%' }}
-					color="inherit"
-					variant="subtitle1"
-					component="div"
-				>
-					{numSelected} selected
-				</Typography>
-			) : (
-				<Typography
-					sx={{ flex: '1 1 100%' }}
-					variant="h6"
-					id="tableTitle"
-					component="div"
-				>
-					Automation Jobs
-				</Typography>
-			)}
-
-			{numSelected > 0 ? (
-				<Tooltip title="Delete">
-					<IconButton>
-						<DeleteIcon />
-					</IconButton>
-				</Tooltip>
-			) : (
-				<Tooltip title="Filter list">
-					<IconButton>
-						<FilterListIcon />
-					</IconButton>
-				</Tooltip>
-			)}
-		</Toolbar>
-	);
-};
-
-TimelineToolbar.propTypes = {
-	numSelected: PropTypes.number.isRequired,
-};
 
 export default function Timeline() {
-	const [order, setOrder] = React.useState('asc');
-	const [orderBy, setOrderBy] = React.useState('topic');
-	const [selected, setSelected] = React.useState([]);
-	const [page, setPage] = React.useState(0);
-	const [dense, setDense] = React.useState(false);
-	const [rowsPerPage, setRowsPerPage] = React.useState(5);
-
-	const handleRequestSort = (event, property) => {
-		const isAsc = orderBy === property && order === 'asc';
-		setOrder(isAsc ? 'desc' : 'asc');
-		setOrderBy(property);
-	};
-
-	const handleSelectAllClick = (event) => {
-		if (event.target.checked) {
-			const newSelecteds = rows.map((n) => n.name);
-			setSelected(newSelecteds);
-			return;
-		}
-		setSelected([]);
-	};
-
-	const handleClick = (event, name) => {
-		const selectedIndex = selected.indexOf(name);
-		let newSelected = [];
-
-		if (selectedIndex === -1) {
-			newSelected = newSelected.concat(selected, name);
-		} else if (selectedIndex === 0) {
-			newSelected = newSelected.concat(selected.slice(1));
-		} else if (selectedIndex === selected.length - 1) {
-			newSelected = newSelected.concat(selected.slice(0, -1));
-		} else if (selectedIndex > 0) {
-			newSelected = newSelected.concat(
-				selected.slice(0, selectedIndex),
-				selected.slice(selectedIndex + 1),
-			);
-		}
-
-		setSelected(newSelected);
-	};
-
-	const handleChangePage = (event, newPage) => {
-		setPage(newPage);
-	};
-
-	const handleChangeRowsPerPage = (event) => {
-		setRowsPerPage(parseInt(event.target.value, 10));
-		setPage(0);
-	};
-
-	const handleChangeDense = (event) => {
-		setDense(event.target.checked);
-	};
-
-	const isSelected = (name) => selected.indexOf(name) !== -1;
-
-	// Avoid a layout jump when reaching the last page with empty rows.
-	const emptyRows =
-		page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
-
 	return (
-		<Box sx={{ width: '100%' }}>
-			<Paper sx={{ width: '100%', mb: 2 }}>
-				<TimelineToolbar numSelected={selected.length} />
-				<TableContainer>
-					<Table
-						sx={{ minWidth: 750 }}
-						aria-labelledby="tableTitle"
-						size={dense ? 'small' : 'medium'}
-					>
-						<TimelineHead
-							numSelected={selected.length}
-							order={order}
-							orderBy={orderBy}
-							onSelectAllClick={handleSelectAllClick}
-							onRequestSort={handleRequestSort}
-							rowCount={rows.length}
-						/>
-						<TableBody>
-							{/* if you don't need to support IE11, you can replace the `stableSort` call with:
-								 rows.slice().sort(getComparator(order, orderBy)) */}
-							{stableSort(rows, getComparator(order, orderBy))
-								.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-								.map((row, index) => {
-									const isItemSelected = isSelected(row.name);
-									const labelId = `timeline-checkbox-${index}`;
-
-									return (
-										<TableRow
-											hover
-											onClick={(event) => handleClick(event, row.name)}
-											role="checkbox"
-											aria-checked={isItemSelected}
-											tabIndex={-1}
-											key={row.name}
-											selected={isItemSelected}
-										>
-											<TableCell padding="checkbox">
-												<Checkbox
-													color="primary"
-													checked={isItemSelected}
-													inputProps={{
-														'aria-labelledby': labelId,
-													}}
-												/>
-											</TableCell>
-											<TableCell
-												component="th"
-												id={labelId}
-												scope="row"
-												padding="none"
-											>
-												{row.name}
-											</TableCell>
-											<TableCell align="right">{row.topic}</TableCell>
-											<TableCell align="right">{row.jobID}</TableCell>
-											<TableCell align="right">{row.lastUpdated}</TableCell>
-											<TableCell align="right">{row.protein}</TableCell>
-										</TableRow>
-									);
-								})}
-							{emptyRows > 0 && (
-								<TableRow
-									style={{
-										height: (dense ? 33 : 53) * emptyRows,
-									}}
-								>
-									<TableCell colSpan={6} />
-								</TableRow>
-							)}
-						</TableBody>
-					</Table>
-				</TableContainer>
-				<TablePagination
-					rowsPerPageOptions={[5, 10, 25]}
-					component="div"
-					count={rows.length}
-					rowsPerPage={rowsPerPage}
-					page={page}
-					onPageChange={handleChangePage}
-					onRowsPerPageChange={handleChangeRowsPerPage}
+		<List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+			<ListItem alignItems="flex-start">
+				<ListItemAvatar>
+					<Avatar alt="A" src="/static/images/avatar/1.jpg" />
+				</ListItemAvatar>
+				<ListItemText
+					primary="Job Completed Successfully"
+					secondary={
+						<React.Fragment>
+							<Typography
+								sx={{ display: 'inline' }}
+								component="span"
+								variant="body2"
+								color="text.primary"
+							>
+							</Typography>
+							{"1 Hour Ago"}
+						</React.Fragment>
+					}
 				/>
-			</Paper>
-			<FormControlLabel
-				control={<Switch checked={dense} onChange={handleChangeDense} />}
-				label="Dense padding"
-			/>
-		</Box>
+			</ListItem>
+			<Divider variant="inset" component="li" />
+			<ListItem alignItems="flex-start">
+				<ListItemAvatar>
+					<Avatar alt="B" src="/static/images/avatar/2.jpg" />
+				</ListItemAvatar>
+				<ListItemText
+					primary="Job Added to Queue"
+					secondary={
+						<React.Fragment>
+							<Typography
+								sx={{ display: 'inline' }}
+								component="span"
+								variant="body2"
+								color="text.primary"
+							>
+							</Typography>
+							{"1 Hour Ago"}
+						</React.Fragment>
+					}
+				/>
+			</ListItem>
+			<Divider variant="inset" component="li" />
+			<ListItem alignItems="flex-start">
+				<ListItemAvatar>
+					<Avatar alt="C" src="/static/images/avatar/3.jpg" />
+				</ListItemAvatar>
+				<ListItemText
+					primary="Job Being Executed"
+					secondary={
+						<React.Fragment>
+							<Typography
+								sx={{ display: 'inline' }}
+								component="span"
+								variant="body2"
+								color="text.primary"
+							>
+							</Typography>
+							{'2 Hours Ago'}
+						</React.Fragment>
+					}
+				/>
+			</ListItem>
+		</List>
 	);
 }
