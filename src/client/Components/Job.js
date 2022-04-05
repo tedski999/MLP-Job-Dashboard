db66ea3bc532cc2ae6df3057c9ca8dea23cbbf80
aoife-K <aoifekhan17@gmail.com>
2022-04-05 14:22:50 +0100

    Move timeline to job page

--- src/client/Components/Job.js
+++ src/client/Components/Job.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Timeline from "./Timeline";
 
 function Job(){
 	return <div className="Job">
         INDIVIDUAL JOB PAGE
+		<div><Timeline></Timeline></div>
 	</div>;
 }
 export default Job;
--- src/client/Components/JobsList.js
+++ src/client/Components/JobsList.js
@@ -1,12 +1,10 @@
 import React from "react";
 import EnhancedTable from "./EnhancedTabled";
-import Timeline from "./Timeline";
 
 function JobsList(){
 	return <div className="Jobs-List">
 		JOBS LIST PAGE
 		<div><EnhancedTable></EnhancedTable></div>
-		<div><Timeline></Timeline></div>
 	</div>;
 }
 export default JobsList;
