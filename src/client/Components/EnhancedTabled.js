e56ea37127ceb929dde4af45d90af9459c39b037
jissj9 <jjojoseph12@gmail.com>
2022-04-05 16:37:23 +0100

    Removed unused function

--- src/client/Components/EnhancedTabled.js
+++ src/client/Components/EnhancedTabled.js
@@ -229,25 +229,25 @@ export default function EnhancedTable() {
 		setSelected([]);
 	};
 
-	const handleClick = (event, name) => {
-		const selectedIndex = selected.indexOf(name);
-		let newSelected = [];
+	// const handleClick = (event, name) => {
+	// 	const selectedIndex = selected.indexOf(name);
+	// 	let newSelected = [];
 
-		if (selectedIndex === -1) {
-			newSelected = newSelected.concat(selected, name);
-		} else if (selectedIndex === 0) {
-			newSelected = newSelected.concat(selected.slice(1));
-		} else if (selectedIndex === selected.length - 1) {
-			newSelected = newSelected.concat(selected.slice(0, -1));
-		} else if (selectedIndex > 0) {
-			newSelected = newSelected.concat(
-				selected.slice(0, selectedIndex),
-				selected.slice(selectedIndex + 1),
-			);
-		}
+	// 	if (selectedIndex === -1) {
+	// 		newSelected = newSelected.concat(selected, name);
+	// 	} else if (selectedIndex === 0) {
+	// 		newSelected = newSelected.concat(selected.slice(1));
+	// 	} else if (selectedIndex === selected.length - 1) {
+	// 		newSelected = newSelected.concat(selected.slice(0, -1));
+	// 	} else if (selectedIndex > 0) {
+	// 		newSelected = newSelected.concat(
+	// 			selected.slice(0, selectedIndex),
+	// 			selected.slice(selectedIndex + 1),
+	// 		);
+	// 	}
 
-		setSelected(newSelected);
-	};
+	// 	setSelected(newSelected);
+	// };
 
 	const handleChangePage = (event, newPage) => {
 		setPage(newPage);
