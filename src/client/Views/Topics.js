b50445b3317134c18322591e2327313d8f8c1bad
jissj9 <jjojoseph12@gmail.com>
2022-04-07 16:17:00 +0100

    Added Layout for Groups Page

--- src/client/Views/Topics.js
+++ src/client/Views/Topics.js
@@ -3,8 +3,28 @@ import React from "react";
 class Topics extends React.Component {
 	render() {
 		return (
-			<div>List of Topics</div>
-		);
+			<div className="topics-main-page-container">
+				<div className="topics-main-page-header">
+					 <h1>
+						 Topic
+					 </h1>
+				</div>
+				<div className="topics-main-page-topics-list">
+					<h3>Topics List</h3>
+					
+					<div>
+						ADD TOPICS LIST
+					</div>
+				</div>
+				<div className="topics-main-page-topics-graph"> 
+					<h3>Topics Graph</h3>
+					<div>
+						ADD TOPICS CHART
+					</div>
+				</div>
+			</div>
+			// <div>List of topics</div>
+		)
 	}
 }
 
