7d5a4ed04f5e01ef8c8252a4937eab5353c9f990
jissj9 <jjojoseph12@gmail.com>
2022-04-07 16:36:35 +0100

    Cleaned Layout of Group and Topic View

--- src/client/Views/Group.js
+++ src/client/Views/Group.js
@@ -9,13 +9,16 @@ export default function Group() {
 			<div className="group-page-header">
 				<h1>Group:{name}</h1>
 			</div>
-			<div className = "group-page-warning-section"> Warning
+			<div className = "group-page-warning-section"> 
+				<h3>Warning</h3>
 				<div>ADD WARNING</div>
 			</div>
-			<div className = "group-page-recentJobs-section"> Recent Jobs
+			<div className = "group-page-recentJobs-section"> 
+				<h3>Recent Jobs</h3>
 				<div> ADD RECENT JOBS </div>
 			</div>
-			<div className = "group-page-history-section"> History
+			<div className = "group-page-history-section">
+				<h3>History</h3>
 				<div>INSERT HISTORY</div>
 			</div>
 		</div>
--- src/client/Views/Topic.js
+++ src/client/Views/Topic.js
@@ -8,13 +8,16 @@ export default function Topic() {
 			<div className="topic-page-header">
 				<h1>Topic:{name}</h1>
 			</div>
-			<div className = "topic-page-warning-section"> Warning
+			<div className = "topic-page-warning-section"> 
+				<h3>Warning</h3>
 				<div>ADD WARNING</div>
 			</div>
-			<div className = "topic-page-recentJobs-section"> Recent Jobs
+			<div className = "topic-page-recentJobs-section"> 
+				<h3>Recent Jobs</h3>
 				<div> ADD RECENT JOBS </div>
 			</div>
-			<div className = "topic-page-history-section"> History
+			<div className = "topic-page-history-section">
+				<h3>History</h3> 
 				<div>INSERT HISTORY</div>
 			</div>
 			{/* <p>Topic: {name}</p> */}
