e214131d1583cf15342cca9402b67e6db94c5a7f
jissj9 <jjojoseph12@gmail.com>
2022-04-07 15:23:59 +0100

    Added Layout for Topic View

--- src/client/Views/Topic.js
+++ src/client/Views/Topic.js
@@ -4,8 +4,21 @@ import { useParams } from "react-router-dom";
 export default function Topic() {
 	const { name } = useParams();
 	return (
-		<div>
-			<p>Topic: {name}</p>
+		<div className = "topic-page-container">
+			<div className="topic-page-header">
+				<h1>Topic:{name}</h1>
+			</div>
+			<div className = "topic-page-warning-section"> Warning
+				<div>ADD WARNING</div>
+			</div>
+			<div className = "topic-page-recentJobs-section"> Recent Jobs
+				<div> ADD RECENT JOBS </div>
+			</div>
+			<div className = "topic-page-history-section"> History
+				<div>INSERT HISTORY</div>
+			</div>
+			{/* <p>Topic: {name}</p> */}
+
 		</div>
 	);
 }
