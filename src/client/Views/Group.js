00e0ad56e62bcb1a666bd13dda54e931db306a81
tedski999 <tedjohnsonjs@gmail.com>
2022-04-08 16:17:51 +0100

    Implemented components in Group and Topic views

--- src/client/Views/Group.js
+++ src/client/Views/Group.js
@@ -1,25 +1,43 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import JobsList from "../Components/Lists/JobsList";
+import StatusOverTime from "../Components/Graphs/StatusOverTime";
+import StatusPieChart from "../Components/Graphs/StatusPieChart";
+import GroupTopicsPieChart from "../Components/Graphs/GroupTopicsPieChart";
+import ExecutionTimeOverTime from "../Components/Graphs/ExecutionTimeOverTime";
 
 export default function Group() {
 	const { name } = useParams();
+	//const now = new Date();
+	const now = new Date("2022-02-10");
+	const oneWeekAgo = new Date(now - 1000*60*60*24*7);
+	const oneMonthAgo = new Date(now - 1000*60*60*24*30);
 	return (
 		<div className = "group-page-container">
-			{/* <p>Group: {name}</p> */}
-			<div className="group-page-header">
-				<h1>Group:{name}</h1>
+			<div className="header">
+				<h1>Group: {name}</h1>
 			</div>
-			<div className = "group-page-warning-section"> 
-				<h3>Warning</h3>
-				<div>ADD WARNING</div>
+			<div className = "graphs">
+				<div className = "graph">
+					<h3>Jobs Last Month</h3>
+					<StatusOverTime after={oneMonthAgo} before={now} group={name} aggregation="day" />
+				</div>
+				<div className = "graph">
+					<h3>Execution Times Last Month</h3>
+					<ExecutionTimeOverTime after={oneMonthAgo} before={now} group={name} aggregation="day" />
+				</div>
+				<div className="graph">
+					<h2>Status Last Week</h2>
+					<StatusPieChart after={oneWeekAgo} before={now} group={name} />
+				</div>
+				<div className="graph">
+					<h2>Topics Last Week</h2>
+					<GroupTopicsPieChart after={oneWeekAgo} before={now} group={name} />
+				</div>
 			</div>
-			<div className = "group-page-recentJobs-section"> 
+			<div className = "recentJobs">
 				<h3>Recent Jobs</h3>
-				<div> ADD RECENT JOBS </div>
-			</div>
-			<div className = "group-page-history-section">
-				<h3>History</h3>
-				<div>INSERT HISTORY</div>
+				<JobsList after={oneMonthAgo} before={now} group={name}/>
 			</div>
 		</div>
 	);
--- src/client/Views/Topic.js
+++ src/client/Views/Topic.js
@@ -1,27 +1,45 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import JobsList from "../Components/Lists/JobsList";
+import StatusOverTime from "../Components/Graphs/StatusOverTime";
+import StatusPieChart from "../Components/Graphs/StatusPieChart";
+import GroupTopicsPieChart from "../Components/Graphs/GroupTopicsPieChart";
+import ExecutionTimeOverTime from "../Components/Graphs/ExecutionTimeOverTime";
 
 export default function Topic() {
 	const { name } = useParams();
+	//const now = new Date();
+	const now = new Date("2022-02-10");
+	const oneWeekAgo = new Date(now - 1000*60*60*24*7);
+	const oneMonthAgo = new Date(now - 1000*60*60*24*30);
 	return (
 		<div className = "topic-page-container">
-			<div className="topic-page-header">
-				<h1>Topic:{name}</h1>
+			<br />
+			<div className="header">
+				<h1>Topic: {name}</h1>
 			</div>
-			<div className = "topic-page-warning-section"> 
-				<h3>Warning</h3>
-				<div>ADD WARNING</div>
+			<div className = "graphs">
+				<div className = "graph">
+					<h3>Last Months History</h3>
+					<StatusOverTime after={oneMonthAgo} before={now} topic={name} aggregation="day" />
+				</div>
+				<div className = "graph">
+					<h3>Last Months Execution Times</h3>
+					<ExecutionTimeOverTime after={oneMonthAgo} before={now} topic={name} aggregation="day" />
+				</div>
+				<div className="graph">
+					<h2>Status Last Week</h2>
+					<StatusPieChart after={oneWeekAgo} before={now} topic={name} />
+				</div>
+				<div className="graph">
+					<h2>Groups Last Week</h2>
+					<GroupTopicsPieChart after={oneWeekAgo} before={now} topic={name} />
+				</div>
 			</div>
-			<div className = "topic-page-recentJobs-section"> 
+			<div className = "recentJobs">
 				<h3>Recent Jobs</h3>
-				<div> ADD RECENT JOBS </div>
+				<JobsList after={oneWeekAgo} before={now} topic={name}/>
 			</div>
-			<div className = "topic-page-history-section">
-				<h3>History</h3> 
-				<div>INSERT HISTORY</div>
-			</div>
-			{/* <p>Topic: {name}</p> */}
-
 		</div>
 	);
 }
